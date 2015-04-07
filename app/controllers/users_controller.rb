b7fbf3f44582005681b71a8da84e65ea03335b24
David Ryan <drsaltmills@gmail.com>
2015-04-07 11:26:36 +0100

    added accept data type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,10 +31,13 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+
     respond_to do |format|
       if @user.save
         sign_in @user 
-        redirect_to lists_path
+        format.html {         
+        redirect_to lists_path, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
