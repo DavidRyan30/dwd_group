673d1c76ed21ef2e28e7e52524f4c8b05d22d48e
David Ryan <drsaltmills@gmail.com>
2015-04-07 11:14:40 +0100

    sign in user on creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,8 +34,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        sign_in @user 
+        redirect_to root_path
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
