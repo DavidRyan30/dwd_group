61071ac61cf05f57e7fe9d482223d6466ba2f0b1
David Ryan <drsaltmills@gmail.com>
2015-04-10 10:11:03 +0100

    cleaning up sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
        def create
         user = User.find_by_email(params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
+          puts "Authenticated"
           sign_in user 
           redirect_to root_path
         else
+          puts "not Atuhenticated"
 			flash.now[:error] = 'Invalid email/password combination'          
 			render 'new'
         end
