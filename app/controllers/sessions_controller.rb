6cc9cd5d0808dbbfad6fb6d1662c2346e18deccf
David Ryan <drsaltmills@gmail.com>
2015-04-10 09:23:20 +0100

    added remember token to the user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,17 @@ class SessionsController < ApplicationController
 
 	end
        def create
+            @session = params[:session]
+            if @session == {}
+              user1 = User.find_by_email(params[:email])
+              @session = user.remember_token
+            end
         user = User.find_by_email(params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
           sign_in user 
-          redirect_to root_path
+          format.html { redirect_to root_path, notice: 'User was successfully updated.' }
+          format.json { render json: user}
+          
         else
 			flash.now[:error] = 'Invalid email/password combination'          
 			render 'new'
